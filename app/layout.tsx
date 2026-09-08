443ae3d4dbc371b4806bb0610356105bb2f25dd6
Cankkun <nguyenduccanh177@gmail.com>
2026-09-08 08:07:16 +0700

    ssss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import SWRegister from "./sw-register";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -36,7 +37,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <SWRegister />
+        {children}
+      </body>
     </html>
   );
 }
